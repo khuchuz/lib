dbc4fe5263daf97b860e9dfe289764d12223566a
Fajar Satria <iamfajarsatria@gmail.com>
2022-08-11 23:31:25 +0700

    Add: Book Controller & View

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/book', 'App\Http\Controllers\BookController@index');
+Route::get('/book/create', 'App\Http\Controllers\BookController@create');
+Route::get('/book/edit', 'App\Http\Controllers\BookController@edit');
+Route::get('/book/update', 'App\Http\Controllers\BookController@update');
+Route::get('/book/delete', 'App\Http\Controllers\BookController@delete');
